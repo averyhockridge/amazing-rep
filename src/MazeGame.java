a97dd0747429f6d602fada7968c1a8d3803e32f1
brando2131 <brands2131@gmail.com>
2014-05-22 01:11:01 +1000

    Update MazeGame.java

--- src/MazeGame.java
+++ src/MazeGame.java
@@ -1,37 +1,43 @@
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-/**
- * 
- * @author Brandon Sandoval
- *
- */
+public class MazeGame implements ActionListener {
 
-public class MazeGame implements java.awt.event.ActionListener{
-	/**
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args){
+	public static void main(String[] args) {
+		run();
+	}
+	
+	public static void run() {
+		StartPanel start;
+		PlayPanel play;
+		EndPanel end;
+		HelpPanel help;
+
+		Difficulty difficulty;
+		int endState;
+		boolean continueGame;
+
+		do{
+			difficulty = start.getDifficulty();
+			start.close();
+
+			play(difficulty);
+			endState = play.getEndState();
+			play.close();
+
+			end(endState);
+			continueGame = end.continueGame();
+			end.close();
+		}
+		while(continueGame);
+	}
+
+	public void actionListener(ActionEvent e) {
 		
 	}
-	/**
-	 * 
-	 */
-	public void run(){
-		
-	}
-	/**
-	 * 
-	 * @param e
-	 */
-	public void ActionListener(ActionEvent e){
-
-	}
-	/**
-	 * 
-	 */
+	
 	@Override
-	public void actionPerformed(ActionEvent e){
+	public void actionPerformed(ActionEvent arg0) {
 		
 	}
 }
