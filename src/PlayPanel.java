c8c19c77e2415ce0bfacb47359cb9ec7d2da1344
brando2131 <brands2131@gmail.com>
2014-05-24 21:07:45 +1000

    Update PlayPanel.java

--- src/PlayPanel.java
+++ src/PlayPanel.java
@@ -1,6 +1,9 @@
 import java.awt.BorderLayout;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
@@ -18,25 +21,37 @@ public class PlayPanel extends JPanel{
 	 */
 	public PlayPanel(int difficulty){
 		JPanel mazePanel = new JPanel();
-		mazePanel.add(new MazeGenerator(difficulty, 200, 200), BorderLayout.CENTER);
-		//mazePanel.setVisible(true);
+		JPanel buttonPanel = new JPanel();
+		JButton end = new JButton("endTest");
+		end.addActionListener(new
+	            ActionListener()
+	            {
+	               public void actionPerformed(ActionEvent event)
+	               {
+	            	    endState = 1;
+	               }
+	            });	
+		end.setVisible(true);	
+		mazePanel.add(new MazeGenerator(difficulty, 200, 200));
+		mazePanel.setVisible(true);
 		this.difficulty = difficulty;
 		endState = 0;
 		location = 0;
 		player = new Player(location, difficulty);
 		
+		buttonPanel.add(end);
+		buttonPanel.setVisible(true);
 		this.add(mazePanel);
+		this.add(end);
 		
 	}
 	
 	public int getEndState(){
-	int ret = endState;
-	return ret;
+		return endState;
 	}
-	/*
+	
 	public void paint(Graphics g) {
 		location = player.getLocation();
-		g.drawOval(location%difficulty, location/difficulty, 2, 2);
-		
-	}*/
+		g.drawOval((location%difficulty)*50, (location/difficulty)*50, 50, 50);
+	}
 }
