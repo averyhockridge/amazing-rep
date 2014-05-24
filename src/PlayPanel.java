06dd33738e27421ff3d603784b285fbedf929211
brando2131 <brands2131@gmail.com>
2014-05-24 22:38:40 +1000

    Update PlayPanel.java

--- src/PlayPanel.java
+++ src/PlayPanel.java
@@ -22,27 +22,41 @@ public class PlayPanel extends JPanel{
 	public PlayPanel(int difficulty){
 		JPanel mazePanel = new JPanel();
 		JPanel buttonPanel = new JPanel();
-		JButton end = new JButton("endTest");
-		end.addActionListener(new
+		JButton winButton= new JButton("Simulate Win");
+		JButton lossButton = new JButton("Simulate Loss");
+		
+		winButton.addActionListener(new
 	            ActionListener()
 	            {
 	               public void actionPerformed(ActionEvent event)
 	               {
-	            	    endState = 1;
+	            	   endState = 1;
 	               }
-	            });	
-		end.setVisible(true);	
+	            });		
+		
+		lossButton.addActionListener(new
+	            ActionListener()
+	            {
+	               public void actionPerformed(ActionEvent event)
+	               {
+	            	   endState = 2;
+	               }
+	            });
+		
+	    buttonPanel.add(winButton);
+	    buttonPanel.add(lossButton);
+		buttonPanel.setVisible(true);	
+	    
 		mazePanel.add(new MazeGenerator(difficulty, 200, 200));
 		mazePanel.setVisible(true);
+		
 		this.difficulty = difficulty;
 		endState = 0;
 		location = 0;
 		player = new Player(location, difficulty);
 		
-		buttonPanel.add(end);
-		buttonPanel.setVisible(true);
 		this.add(mazePanel);
-		this.add(end);
+		this.add(buttonPanel);
 		
 	}
 	
@@ -52,6 +66,11 @@ public class PlayPanel extends JPanel{
 	
 	public void paint(Graphics g) {
 		location = player.getLocation();
-		g.drawOval((location%difficulty)*50, (location/difficulty)*50, 50, 50);
+		if(difficulty == 10) {
+			g.drawOval((location%difficulty)*50, (location/difficulty)*50, 50, 50);
+		}
+		else if(difficulty == 20) {
+			g.drawOval((location%difficulty)*25, (location/difficulty)*25, 25, 25);	
+		}
 	}
 }
