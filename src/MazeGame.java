178ae34411344500e1789a7532d22f3908e2b4ce
brando2131 <brands2131@gmail.com>
2014-05-24 21:07:03 +1000

    Update MazeGame.java

--- src/MazeGame.java
+++ src/MazeGame.java
@@ -1,86 +1,101 @@
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * @author Stuart Aitken, Brandon Sandoval, Leah Williams
  *
  */
 public class MazeGame implements ActionListener {
+
+	private final static int START = 0;
+	private final static int PLAY = 1;
+	private final static int END = 2;
+	private final static int QUIT = 3;
 	private static int state;
-	static MazeGame maze = new MazeGame(0);
+	private int difficulty = 0;
+	static MazeGame maze = new MazeGame(START);
+	
 	public static void main(String[] args) {
-		maze.run();
+		while(maze.run());
 	}
 	
 	public MazeGame(int state){
 		this.state = state;
 	}
 	
-	public void run() {
-		StartPanel start = new StartPanel();
-		PlayPanel play = new PlayPanel();
-		EndPanel end = new EndPanel(1);
-		HelpPanel help = new HelpPanel();
+	public boolean run() {
+		StartPanel start = null;
+		PlayPanel play = null;
+		MazeGenerator mazeGen = null;
+		EndPanel end = null;
+		HelpPanel help = null;
 
-		int difficulty = 0;
-		int endState;
+		boolean ret;
+		int endState = 0;
 		int continueGame = 0;
 		
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(750, 500);
 	    frame.setVisible(true);
-	
-	    //if(state != 10){
-	    	while(state != 10){
-				if(state == 0){
-				    frame.add(start, BorderLayout.CENTER);
-					end.setVisible(false);
-					start.setVisible(true);
-					while(difficulty == 0) {
-						difficulty = start.getDifficulty();		
-					}
-					state = 1;
-					continueGame = 0;
-				}
-				else if(state == 1){
-					//TODO Still need a working PlayPanel
-				    frame.add(new MazeGenerator(10, 100, 100), BorderLayout.CENTER);
-					end.setVisible(false);
-					start.setVisible(false);
-					while(true){}
-					
-				}
-				else if(state == 2){
-				    frame.add(end, BorderLayout.CENTER);
-					System.out.println("end");
-					end = new EndPanel(1);
-					start.setVisible(false);
-					end.setVisible(true);
-					while(continueGame == 0) {
-						continueGame = end.getContinueState();
-					}
-					if(continueGame == 2)
-						state = 10;
-				}
-				frame.dispose();
-				run();
-				System.out.println(difficulty);
 
-				//frame.add(play, BorderLayout.CENTER);
-				//play.play(difficulty);
-				//endState = play.getEndState();
+		if(state == START) {
+			start = new StartPanel();
+			start.setVisible(true);
+			frame.add(start);
+			frame.setVisible(true);
+			while(difficulty == 0) {
+				difficulty = start.getDifficulty();	
+				System.out.println("loop1");	
+			}
+			state = PLAY;
+		}
+		else if(state == PLAY) {			
+			play = new PlayPanel(difficulty);
+			play.setVisible(true);
+			
+			mazeGen = new MazeGenerator(difficulty, 500, 500);
+			mazeGen.setVisible(true);
 
-				//end = new EndPanel();
-				//frame.add(end, BorderLayout.CENTER);
-				//end(endState);
-				//continueGame = end.continueGame();
-				
-			};
-	    //}
+			frame.add(play, BorderLayout.NORTH);
+			frame.add(mazeGen, BorderLayout.CENTER);
+			
+			frame.setVisible(true);
+
+			while(endState == 0){
+				endState = play.getEndState();	
+				System.out.println("loop2");
+			}
+			difficulty = 0;
+			state = END;
+		}
+		else if(state == END) {
+			end = new EndPanel(1);
+		    frame.add(end, BorderLayout.CENTER);
+			end.setVisible(true);
+			while(continueGame == 0) {
+				continueGame = end.getContinueState();	
+				System.out.println("loop3");
+			}
+			if(continueGame == 2) {
+				state = QUIT;
+			}
+			else{
+				state = START;
+			}
+		}
+		
+		frame.dispose();
+		if(state == QUIT) {
+			ret = false;
+		}
+		else{
+			ret = true;
+		}
+		return ret;
 	}
 
 	public void actionListener(ActionEvent e) {
