7d0667999170c424be4a7281059cd63d813ec0e2
Triptych03 <Seaitken@hotmail.com>
2014-05-27 21:08:54 +1000

    remove test buttons

--- src/PlayPanel.java
+++ src/PlayPanel.java
@@ -8,6 +8,7 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 
 /**
@@ -31,6 +32,8 @@ public class PlayPanel extends JPanel{
 	public PlayPanel(int difficulty, MazeGenerator maze){
 
 		mazePanel = new JPanel();
+		
+		/*
 		JPanel buttonPanel = new JPanel();
 		JButton winButton= new JButton("Simulate Win");
 		JButton lossButton = new JButton("Simulate Loss");
@@ -56,22 +59,23 @@ public class PlayPanel extends JPanel{
 	    buttonPanel.add(winButton);
 	    buttonPanel.add(lossButton);
 		buttonPanel.setVisible(true);
+		**/
 		
-		MazeGenerator mg = maze;
-		mg.setVisible(true);
+		//MazeGenerator mazeGen = new MazeGenerator(difficulty, 500, 500);
+		//mazeGen.setVisible(true);
 		
-		mazePanel.add(mg);
-		mazePanel.setVisible(true);
+		//mazePanel.add(mazeGen);
+		//mazePanel.setVisible(true);
 				
 		
 		this.difficulty = difficulty;
 		endState = 0;
 		int playerLocation = 0;
-		player = new Player(playerLocation, difficulty, mg);
+		player = new Player(playerLocation, difficulty, maze);
 		
 		
-		this.add(maze, BorderLayout.NORTH);
-		this.add(buttonPanel, BorderLayout.EAST);
+		//this.add(mazePanel, BorderLayout.NORTH);
+		//this.add(buttonPanel, BorderLayout.EAST);
 		this.setBackground(new Color(100,200,150,100));
 		
 	}
@@ -87,15 +91,17 @@ public class PlayPanel extends JPanel{
 
 	public void paint(Graphics g) {
 		super.paint(g);
-		mazePanel.paint(g);
+		
 		int location = player.getLocation();
 		if(difficulty == 10) {
 			//g.drawOval((location%difficulty)*50, (location/difficulty)*50, 50, 50);
 			g.drawImage(player.getImage(), (location%difficulty)*50, (location/difficulty)*50, null);
 		}
 		else if(difficulty == 20) {
-			//g.drawOval((location%difficulty)*25, (location/difficulty)*25, 25, 25);	
+			//g.drawOval((location%difficulty)*25, (location/difficulty)*25, 25, 25);
+			g.drawImage(player.getImage(), (location%difficulty)*50, (location/difficulty)*50, null);
 		}
+		mazePanel.paint(g);
 	}
 	
 	
