77d76d3acf98f266f6a337159dc5cd98a8e63ba1
Triptych03 <Seaitken@hotmail.com>
2014-05-30 15:28:51 +1000

    added button icons & layout managers, needs fixing

--- src/StartPanel.java
+++ src/StartPanel.java
@@ -1,58 +1,98 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-
 /**
-* @author Stuart Aitken
-*/
-@SuppressWarnings("serial")
-public class StartPanel extends JPanel{
+ * @author Stuart Aitken
+ */
+public class StartPanel extends JPanel {
 
 	private int difficulty = 0;
-	
-	public StartPanel(){
-		JPanel messagePanel = new JPanel();
-		this.add(new JLabel("Choose your difficulty to start the game"));
-		
+	private Image background;
+
+	public StartPanel() {
+		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		JLabel message = new JLabel("Choose your difficulty to start the game");
+		message.setFont(new Font("Serif", Font.PLAIN, 30));
+		message.setForeground(new Color(0, 255, 0, 150));
+		this.add(message);
+
+		try {
+			BufferedImage img = ImageIO.read(new File(
+					"resources/background.jpg"));
+			background = img;
+		} catch (IOException ex) {
+		}
+
 		JPanel buttonPanel = new JPanel();
-		JButton easyButton= new JButton("Easy");
-		JButton hardButton = new JButton("Hard");
-		
-		easyButton.addActionListener(new
-	            ActionListener()
-	            {
-	               public void actionPerformed(ActionEvent event)
-	               {
-	            	   difficulty = 10;
-	               }
-	            });		
-		
-		hardButton.addActionListener(new
-	            ActionListener()
-	            {
-	               public void actionPerformed(ActionEvent event)
-	               {
-	            	   difficulty = 20;
-	               }
-	            });
-		
-	    buttonPanel.add(easyButton);
-	    buttonPanel.add(hardButton);	
-	    
-	    this.add(messagePanel);
-	    this.add(buttonPanel);
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+		JButton easyButton = new JButton();
+		try {
+			BufferedImage img = ImageIO.read(new File(
+					"resources/easybutton.jpg"));
+			easyButton.setIcon(new ImageIcon(img));
+		} catch (IOException ex) {
+		}
+		easyButton.setBorder(null);
+		buttonPanel.add(easyButton);
+
+		JButton hardButton = new JButton();
+		try {
+			BufferedImage img = ImageIO.read(new File(
+					"resources/hardbutton.jpg"));
+			hardButton.setIcon(new ImageIcon(img));
+		} catch (IOException ex) {
+		}
+		hardButton.setBorder(null);
+		buttonPanel.add(hardButton);
+
+		easyButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				difficulty = 10;
+			}
+		});
+
+		hardButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				difficulty = 20;
+			}
+		});
+
+		this.add(buttonPanel);
 	}
 
 	/**
 	 * get the chosen difficulty setting
-	 * @return difficulty 
+	 * 
+	 * @return difficulty
 	 */
-	public int getDifficulty(){
+	public int getDifficulty() {
 		int ret = difficulty;
-		return ret;	
+		return ret;
+	}
+
+	/**
+	 * This draws the maze with simple lines, we will need to import graphics
+	 * when we decide the technical details of the GUI
+	 */
+	@Override
+	public void paintComponent(Graphics g) {
+		g.drawImage(background, 0, 0, null);
 	}
 }
