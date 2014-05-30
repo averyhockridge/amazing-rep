d275de966cdbaaf50f6bdb973113dda32158ec5a
brando2131 <brands2131@gmail.com>
2014-05-30 18:33:17 +1000

    Create bar.java

---
+++ src/bar.java
@@ -0,0 +1,27 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+
+public class Bar extends JPanel{
+	Image barImage = null;
+	
+	public Bar(){
+
+		try {
+			BufferedImage img = ImageIO.read(new File("resources/bar.jpg"));
+			barImage = img;
+		} catch (IOException ex){
+			
+		}
+	}
+	
+	public void paint(Graphics g){
+		g.drawImage(barImage, 0, 0, null);
+	}
+}
